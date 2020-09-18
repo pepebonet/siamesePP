df53e2e1ce4c0a76311a7adc3a1b50b9653232a9
jbonet <jbonet@login01>
2020-09-18 16:22:47 +0200

    siamese NN implemented

---
+++ siamesepp/model.py
@@ -0,0 +1,45 @@
+#!usr/bin/env python3
+import numpy as np
+
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras import backend as K
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Input, Lambda, Dense
+
+
+
+def initialize_bias(shape, dtype=None):
+    return K.variable(np.random.normal(loc = 0.5, scale = 1e-2, size = shape), dtype)
+
+
+def get_alternative_siamese(input_shape):
+    # Define the tensors for the two input images
+    left_input = Input(input_shape)
+    right_input = Input(input_shape)
+
+    model = Sequential()
+    model.add(tf.keras.layers.InputLayer(input_shape=input_shape))
+    model.add(tf.keras.layers.Conv1D(256, 5, activation='relu'))
+    model.add(tf.keras.layers.LocallyConnected1D(128, 3, activation='relu'))
+    model.add(tf.keras.layers.Conv1D(256, 3, activation='relu'))
+    model.add(tf.keras.layers.LocallyConnected1D(128, 3, activation='relu'))
+    model.add(tf.keras.layers.GlobalAveragePooling1D(name='seq_pooling_layer'))
+    model.add(tf.keras.layers.Dropout(0.2))
+
+    # Generate the encodings (feature vectors) for the two images
+    encoded_l = model(left_input)
+    encoded_r = model(right_input)
+    
+    # Add a customized layer to compute the absolute difference between the encodings
+    L1_layer = Lambda(lambda tensors:K.abs(tensors[0] - tensors[1]))
+    L1_distance = L1_layer([encoded_l, encoded_r])
+    
+    # Add a dense layer with a sigmoid unit to generate the similarity score
+    prediction = Dense(1, activation='sigmoid', bias_initializer=initialize_bias)(L1_distance)
+    
+    # Connect the inputs with the outputs
+    siamese_net = Model(inputs=[left_input,right_input],outputs=prediction)
+    
+    # return the model
+    return siamese_net
