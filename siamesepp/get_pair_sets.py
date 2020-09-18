bca88d1d3b3c0e3611f4a85a33261cf74fad9adc
jbonet <jbonet@login01>
2020-09-18 16:18:47 +0200

    unsupervised pair extraction initialize

--- siamesepp/get_pair_sets.py
+++ siamesepp/get_pair_sets.py
@@ -8,6 +8,8 @@ import tensorflow as tf
 from collections import Counter
 from sklearn.model_selection import train_test_split
 
+from tqdm import tqdm
+
 
 names_all=['chrom', 'pos', 'strand', 'pos_in_strand', 'readname', 
             'read_strand', 'kmer', 'signal_means', 'signal_stds', 'signal_median',
@@ -29,7 +31,7 @@ def get_equals_set(df):
     to_analyze = [el for el in c.elements() if c[el] >= 2]
     
     arr1 = np.empty([0,], dtype='int32'); arr2 = np.empty([0,], dtype='int32')
-    for el in list(set(to_analyze)):
+    for el in tqdm(list(set(to_analyze))):
         pos = np.argwhere(df['pos_in_strand'].values == el).flatten()  
         arr1 = np.append(arr1, np.asarray(pos[:int(len(pos) / 2)]))
         arr2 = np.append(arr2, np.asarray(pos[int(len(pos) / 2):]))
@@ -43,7 +45,7 @@ def get_training_test_val(df):
     return [(train, 'train'), (test, 'test'), (val, 'val')]
 
 
-def preprocess_sequence(df, output, file, pair):
+def preprocess_sequence(df, output, data_type, file, pair):
     df = df.dropna()
     kmer = df['kmer_{}'.format(pair)].apply(kmer2code)
     base_mean = [tf.strings.to_number(i.split(','), tf.float32) \
@@ -60,7 +62,8 @@ def preprocess_sequence(df, output, file, pair):
         for i in df['signal_diff_{}'.format(pair)].values]
     base_signal_len = [tf.strings.to_number(i.split(','), tf.float32) \
         for i in df['signal_lens_{}'.format(pair)].values]
-    label = df['label']
+    if data_type != 'unsup':
+        label = df['label']
 
     file_name = os.path.join(output, '{}_seq.h5'.format(file))
 
@@ -73,12 +76,21 @@ def preprocess_sequence(df, output, file, pair):
         hf.create_dataset("signal_kurt_{}".format(pair),  data=np.stack(base_kurt))
         hf.create_dataset("signal_diff_{}".format(pair),  data=np.stack(base_diff))
         hf.create_dataset("signal_lens_{}".format(pair),  data=np.stack(base_signal_len))
-        hf.create_dataset("label_{}".format(pair),  data=label)
+        if data_type != 'unsup':
+            hf.create_dataset("label_{}".format(pair),  data=label)
 
     return None
 
 
 @click.command(short_help='Script to separate files per position')
+@click.option(
+    '-dt', '--data-type', required=True,
+    type=click.Choice(['sup', 'unsup']),
+    help='Supervised '
+)
+@click.option(
+    '-ud', '--unsupervised-data', help='unsupervised data file'
+)
 @click.option(
     '-t', '--treated', help='treated file'
 )
@@ -88,25 +100,34 @@ def preprocess_sequence(df, output, file, pair):
 @click.option(
     '-o', '--output', default=''
 )
-def main(treated, untreated, output):
-    treated = get_data(treated)
-    untreated = get_data(untreated)
+def main(data_type, unsupervised_data, treated, untreated, output):
+    if data_type == 'sup':
+
+        treated = get_data(treated)
+        untreated = get_data(untreated)
+        
+        treat_untreat = pd.merge(treated, untreated, on=['pos_in_strand'], how='inner')
+        treat_untreat['label'] = 0
+
+        treat_treat = get_equals_set(treated)
+        treat_treat['label'] = 1
+        
+        untreat_untreat = get_equals_set(untreated)
+        untreat_untreat['label'] = 1
+
+        df = pd.concat([treat_untreat, treat_treat, untreat_untreat])
+        data = get_training_test_val(df)
+
+        for el in data:
+            preprocess_sequence(el[0], output, el[1], 'x')
+            preprocess_sequence(el[0], output, el[1], 'y') 
     
-    treat_untreat = pd.merge(treated, untreated, on=['pos_in_strand'], how='inner')
-    treat_untreat['label'] = 0
+    else:
+        df = get_data(unsupervised_data)
+        df_pairs = get_equals_set(df)
 
-    treat_treat = get_equals_set(treated)
-    treat_treat['label'] = 1
-    
-    untreat_untreat = get_equals_set(untreated)
-    untreat_untreat['label'] = 1
-
-    df = pd.concat([treat_untreat, treat_treat, untreat_untreat])
-    data = get_training_test_val(df)
-
-    for el in data:
-        preprocess_sequence(el[0], output, el[1], 'x')
-        preprocess_sequence(el[0], output, el[1], 'y') 
+        preprocess_sequence(df_pairs, output, data_type, 'mms', 'x')
+        preprocess_sequence(df_pairs, output, data_type, 'mms', 'y') 
 
 
 if __name__ == '__main__':
