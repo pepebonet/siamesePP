dc7bbdeb9c2e2d80d29c804e6e3fff800bc96206
josebonet <jose.bonet@irbbarcelona.org>
2021-02-22 14:42:23 +0100

    load model not weights

--- siamesepp/call_modifications.py
+++ siamesepp/call_modifications.py
@@ -11,13 +11,6 @@ import siamesepp.utils as ut
 import siamesepp.model as md
 
 
-def load_model(model_file, kmer, feat):
-    model = md.get_alternative_siamese((kmer, feat))
-    model.load_weights(os.path.join(model_file, "weights.h5"))
-    model.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(), metrics=['accuracy'])
-    return model
-
-
 def acc_test_single(data, labels, model, kmer, feat=12, score_av='binary'):
     test_loss, test_acc = model.evaluate(data, tf.convert_to_tensor(labels))
 
@@ -28,7 +21,7 @@ def acc_test_single(data, labels, model, kmer, feat=12, score_av='binary'):
     precision, recall, f_score, _ = precision_recall_fscore_support(
         labels, inferred, average=score_av
     )
-
+    
     return [test_acc, precision, recall, f_score], pred, inferred
 
 
@@ -42,28 +35,13 @@ def infer_mods(data, model):
 
 
 def call_mods(data_type, test_file, model_file, kmer, output, feat=12):
-    data, labels = ut.get_pairs(test_file, kmer, data_type)
-    model = load_model(model_file, kmer, feat)
+    data, labels = ut.get_pairs(test_file, kmer)
+    model = load_model(model_file)
 
     if data_type == 'sup':
         acc, pred, inferred = acc_test_single(data, labels, model, kmer)
-        save_output(acc, output, 'accuracy_measurements.txt')
+        ut.save_output(acc, output, 'accuracy_measurements.txt')
     else:
         pred, inferred = infer_mods(data, model)
     
-    save_probs(pred, inferred, labels, output)
-
-
-def save_probs(probs, inferred, labels, output):
-    out_probs = os.path.join(output, 'test_pred_prob.txt')
-    probs_to_save = pd.DataFrame(columns=['labels', 'probs', 'inferred'])
-    probs_to_save['labels'] = labels
-    probs_to_save['probs'] = probs
-    probs_to_save['inferred'] = inferred
-    probs_to_save.to_csv(out_probs, sep='\t', index=None)
-
-
-def save_output(acc, output, label):
-    col_names = ['Accuracy', 'Precision', 'Recall', 'F-score']
-    df = pd.DataFrame([acc], columns=col_names)
-    df.to_csv(os.path.join(output, label), index=False, sep='\t')
+    ut.save_probs(pred, inferred, labels, output)
