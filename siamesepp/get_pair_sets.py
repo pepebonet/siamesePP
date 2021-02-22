fd7bff6af0eb3051700c99bfe2daf8dc219d639f
josebonet <jose.bonet@irbbarcelona.org>
2021-02-22 10:27:55 +0100

    merge through id

--- siamesepp/get_pair_sets.py
+++ siamesepp/get_pair_sets.py
@@ -4,39 +4,61 @@ import h5py
 import click
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 import tensorflow as tf
 from collections import Counter
 from sklearn.model_selection import train_test_split
 
-from tqdm import tqdm
+import siamesepp.utils as ut
 
-
-names_all=['chrom', 'pos', 'strand', 'pos_in_strand', 'readname', 
+#TODO change to only seq
+names_all = ['chrom', 'pos', 'strand', 'pos_in_strand', 'readname',
             'read_strand', 'kmer', 'signal_means', 'signal_stds', 'signal_median',
-            'signal_skew', 'signal_kurt', 'signal_diff', 'signal_lens', 
-            'methyl_label', 'flag']
+            'signal_diff', 'qual', 'mis', 'ins', 'del', 'methyl_label']
+
+names_seq = ['chrom', 'pos', 'strand', 'pos_in_strand', 'readname',
+            'read_strand', 'kmer', 'signal_means', 'signal_stds', 'signal_median',
+            'signal_diff', 'methyl_label']
+
 base2code_dna = {'A': 0, 'C': 1, 'G': 2, 'T': 3, 'N': 4}
 
+
+# ------------------------------------------------------------------------------
+# FUNCTIONS
+# ------------------------------------------------------------------------------
 
 def kmer2code(kmer_bytes):
     return [base2code_dna[x] for x in kmer_bytes]
 
 
-def get_data(treated):
-    return pd.read_csv(treated, sep='\t', header=None, names=names_all)
+def get_data(path):
+    df = pd.read_csv(path, sep='\t', header=None, names=names_all)
+    df['id'] = df['chrom'] + '_' + df['pos'].astype(str) + '_' + df['strand']
+
+    return df
 
 
 def get_equals_set(df):
-    c = Counter(df['pos_in_strand'])
+    c = Counter(df['id'])
     to_analyze = [el for el in c.elements() if c[el] >= 2]
-    
-    arr1 = np.empty([0,], dtype='int32'); arr2 = np.empty([0,], dtype='int32')
-    for el in tqdm(list(set(to_analyze))):
-        pos = np.argwhere(df['pos_in_strand'].values == el).flatten()  
-        arr1 = np.append(arr1, np.asarray(pos[:int(len(pos) / 2)]))
-        arr2 = np.append(arr2, np.asarray(pos[int(len(pos) / 2):]))
 
-    return pd.merge(df.iloc[arr1, :], df.iloc[arr2, :], on=['pos_in_strand'], how='inner')
+    if to_analyze:
+        arr1 = np.empty([0,], dtype='int32')
+        arr2 = np.empty([0,], dtype='int32')
+
+        for el in tqdm(list(set(to_analyze))):
+
+            pos = np.argwhere(df['id'].values == el).flatten()  
+            arr1 = np.append(arr1, np.asarray(pos[:int(len(pos) / 2)]))
+            arr2 = np.append(arr2, np.asarray(pos[int(len(pos) / 2):]))
+
+        merged = pd.merge(df.iloc[arr1, :], df.iloc[arr2, :], on=['id'], how='inner')
+        emrged['label'] = 1
+
+        return merged
+    
+    else:
+        return pd.DataFrame()
 
 
 def get_training_test_val(df):
@@ -45,62 +67,27 @@ def get_training_test_val(df):
     return [(train, 'train'), (test, 'test'), (val, 'val')]
 
 
-def preprocess_sequence(df, output, data_type, file, pair):
-    df = df.dropna()
-    kmer = df['kmer_{}'.format(pair)].apply(kmer2code)
-    base_mean = [tf.strings.to_number(i.split(','), tf.float32) \
-        for i in df['signal_means_{}'.format(pair)].values]
-    base_std = [tf.strings.to_number(i.split(','), tf.float32) \
-        for i in df['signal_stds_{}'.format(pair)].values]
-    base_median = [tf.strings.to_number(i.split(','), tf.float32) \
-        for i in df['signal_median_{}'.format(pair)].values]
-    base_skew = [tf.strings.to_number(i.split(','), tf.float32) \
-        for i in df['signal_skew_{}'.format(pair)].values]
-    base_kurt = [tf.strings.to_number(i.split(','), tf.float32) \
-        for i in df['signal_kurt_{}'.format(pair)].values]
-    base_diff = [tf.strings.to_number(i.split(','), tf.float32) \
-        for i in df['signal_diff_{}'.format(pair)].values]
-    base_signal_len = [tf.strings.to_number(i.split(','), tf.float32) \
-        for i in df['signal_lens_{}'.format(pair)].values]
-    if data_type != 'unsup':
-        label = df['label']
-
-    file_name = os.path.join(output, '{}_seq.h5'.format(file))
-
-    with h5py.File(file_name, 'a') as hf:
-        hf.create_dataset("kmer_{}".format(pair),  data=np.stack(kmer))
-        hf.create_dataset("signal_means_{}".format(pair),  data=np.stack(base_mean))
-        hf.create_dataset("signal_stds_{}".format(pair),  data=np.stack(base_std))
-        hf.create_dataset("signal_median_{}".format(pair),  data=np.stack(base_median))
-        hf.create_dataset("signal_skew_{}".format(pair),  data=np.stack(base_skew))
-        hf.create_dataset("signal_kurt_{}".format(pair),  data=np.stack(base_kurt))
-        hf.create_dataset("signal_diff_{}".format(pair),  data=np.stack(base_diff))
-        hf.create_dataset("signal_lens_{}".format(pair),  data=np.stack(base_signal_len))
-        if data_type != 'unsup':
-            hf.create_dataset("label_{}".format(pair),  data=label)
-
-    return None
-
-
 def do_supervised(treated, untreated, data_type, output):
     treated = get_data(treated)
     untreated = get_data(untreated)
-    
-    treat_untreat = pd.merge(treated, untreated, on=['pos_in_strand'], how='inner')
+
+    treat_untreat = pd.merge(treated, untreated, on=['id'], how='inner')
     treat_untreat['label'] = 0
 
     treat_treat = get_equals_set(treated)
-    treat_treat['label'] = 1
     
     untreat_untreat = get_equals_set(untreated)
-    untreat_untreat['label'] = 1
 
     df = pd.concat([treat_untreat, treat_treat, untreat_untreat])
+    import pdb; pdb.set_trace()
+    if df.empty:
+        raise Exception('No features could be extracted...')
+
     data = get_training_test_val(df)
 
     for el in data:
-        preprocess_sequence(el[0], output, data_type, el[1], 'x')
-        preprocess_sequence(el[0], output, data_type, el[1], 'y') 
+        ut.preprocess_sequence(el[0], output, data_type, el[1], 'x')
+        ut.preprocess_sequence(el[0], output, data_type, el[1], 'y') 
 
 
 def do_unsupervised(treated, untreated, data_type, output):
@@ -112,5 +99,5 @@ def do_unsupervised(treated, untreated, data_type, output):
 
     df_pairs = pd.merge(treated, untreated, on='id', how='inner')
 
-    preprocess_sequence(df_pairs, output, data_type, 'mms_untreated', 'x')
-    preprocess_sequence(df_pairs, output, data_type, 'mms_untreated', 'y') 
+    ut.preprocess_sequence(df_pairs, output, data_type, 'mms_untreated', 'x')
+    ut.preprocess_sequence(df_pairs, output, data_type, 'mms_untreated', 'y') 
