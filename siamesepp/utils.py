d2d5ca5b0901dff140ea5d870047fba1da7dd2e5
josebonet <jose.bonet@irbbarcelona.org>
2021-02-22 11:52:40 +0100

    modify data process for training based on new features

--- siamesepp/utils.py
+++ siamesepp/utils.py
@@ -9,70 +9,79 @@ base2code_dna = {'A': 0, 'C': 1, 'G': 2, 'T': 3, 'N': 4}
 def kmer2code(kmer_bytes):
     return [base2code_dna[x] for x in kmer_bytes]
 
-def load_data(file, pair, data_type):
+
+# ------------------------------------------------------------------------------
+# TRAIN AND CALL MODIFICATIONS
+# ------------------------------------------------------------------------------
+
+def load_seq_data(file, pair):
 
     with h5py.File(file, 'r') as hf:
         bases = hf['kmer_{}'.format(pair)][:]
         signal_means = hf['signal_means_{}'.format(pair)][:]
         signal_stds = hf['signal_stds_{}'.format(pair)][:]
-        signal_median = hf['signal_median_{}'.format(pair)][:]
-        signal_skew = hf['signal_skew_{}'.format(pair)][:]
-        signal_kurt = hf['signal_kurt_{}'.format(pair)][:]
-        signal_diff = hf['signal_diff_{}'.format(pair)][:]
-        signal_lens = hf['signal_lens_{}'.format(pair)][:]
-        if data_type == 'sup':
-            label = hf['label_{}'.format(pair)][:]
-        else: 
-            label = np.zeros(signal_means.shape[0])
+        signal_medians = hf['signal_median_{}'.format(pair)][:]
+        signal_range = hf['signal_diff_{}'.format(pair)][:]
+        label = hf['methyl_label_{}'.format(pair)][:]
+        chrom = hf['chrom_{}'.format(pair)][:]
+        readname = hf['readname_{}'.format(pair)][:]
+        pos = hf['pos_{}'.format(pair)][:]
+        strand = hf['strand_{}'.format(pair)][:]
+        pos_in_strand = hf['pos_in_strand_{}'.format(pair)][:]
 
-    return bases, signal_means, signal_stds, signal_median, signal_skew, \
-        signal_kurt, signal_diff, signal_lens, label
+    return bases, signal_means, signal_stds, signal_medians, \
+        signal_range, label, chrom, readname, pos, strand, pos_in_strand
 
 
 def get_feat_pairs(g1, g2): 
     pairs = [np.zeros((g1.shape[0], g1.shape[1], g1.shape[2])) for i in range(2)]
+    
     for i in range(g1.shape[0]):
         pairs[0][i,:,:] = g1[i]
         pairs[1][i,:,:] = g2[i]
+    
     return pairs
 
 
-def concat_tensors(bases, v2, v3, v4, v5, v6, v7, v8, kmer):
-    return tf.concat([bases, 
-                                tf.reshape(v2, [-1, kmer, 1]),
-                                tf.reshape(v3, [-1, kmer, 1]),
-                                tf.reshape(v4, [-1, kmer, 1]),
-                                tf.reshape(v5, [-1, kmer, 1]),
-                                tf.reshape(v6, [-1, kmer, 1]),
-                                tf.reshape(v7, [-1, kmer, 1]),
-                                tf.reshape(v8, [-1, kmer, 1])],
+def concat_tensors_seq(bases, signal_means, signal_stds, signal_medians,
+                        signal_range, kmer):
+    return tf.concat([bases,
+                                tf.reshape(signal_means, [-1, kmer, 1]),
+                                tf.reshape(signal_stds, [-1, kmer, 1]),
+                                tf.reshape(signal_medians, [-1, kmer, 1]),
+                                tf.reshape(signal_range, [-1, kmer, 1])],
                                 axis=2)
 
 
-def get_pairs(feat_file, kmer_sequence, data_type='sup'):
-    embedding_flag = ""
+def get_pairs(feat_file, kmer):
 
     ## preprocess data
-    v1_x, v2_x, v3_x, v4_x, v5_x, v6_x, v7_x, v8_x, vy_x  = load_data(feat_file, 'x', data_type)
-    v1_y, v2_y, v3_y, v4_y, v5_y, v6_y, v7_y, v8_y, vy_y  = load_data(feat_file, 'y', data_type)
+    bases_x, signal_means_x, signal_stds_x, signal_medians_x, \
+            signal_range_x, label_x, chrom_x, readname_x, pos_x, strand_x, \
+                pos_in_strand_x  = load_seq_data(feat_file, 'x')
+
+    bases_y, signal_means_y, signal_stds_y, signal_medians_y, \
+            signal_range_y, label_y, chrom_y, readname_y, pos_y, strand_y, \
+                pos_in_strand_y  = load_seq_data(feat_file, 'y')
 
     embedding_size = 5
-    embedding_flag += "_one-hot_embedded"
-    bases = tf.one_hot(v1_x, embedding_size)
+    bases = tf.one_hot(bases_x, embedding_size)
 
     ## prepare inputs for NNs
-    input_x = concat_tensors(
-        bases, v2_x, v3_x, v4_x, v5_x, v6_x, v7_x, v8_x, kmer_sequence
-    )
-    input_y = concat_tensors(
-        bases, v2_y, v3_y, v4_y, v5_y, v6_y, v7_y, v8_y, kmer_sequence
-    )
+    input_x = concat_tensors_seq(bases, signal_means_x, signal_stds_x, 
+        signal_medians_x, signal_range_x, kmer)
+    
+    input_y = concat_tensors_seq(bases, signal_means_y, signal_stds_y, 
+        signal_medians_y, signal_range_y, kmer)
 
     pairs = get_feat_pairs(input_x, input_y)
 
-    return pairs, vy_x
+    return pairs, label_x
 
 
+# ------------------------------------------------------------------------------
+#  PREPROCESS
+# ------------------------------------------------------------------------------
 
 def preprocess_sequence(df, output, label_file, file, pair):
 
